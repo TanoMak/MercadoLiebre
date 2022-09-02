6fd3f2126d93c021fe2805759db627cb426e6392
TanoMak <martinmacchia@gmail.com>
2022-09-02 10:05:34 -0300

    heroku connect

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ app.use(express.static('public'));
 
 app.set("view engine", "ejs")
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT ||3000, ()=>{
+    console.log('Servidor corriendo en el puerto 3000');
 });
 
 
